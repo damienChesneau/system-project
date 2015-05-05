9ffbb015f8addb7e04c727d755f840b5ed5a9ffb
Leynere <steeve.sivanantham@gmail.com>
2015-05-05 23:04:15 +0200

    Begin of the implementation of sockets

--- src/level_1.c
+++ src/level_1.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<getopt.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include <netinet/in.h>
 
 /*
 Level 1: Each repository has an execution of this program (multiple instances) and we must synchronize them.
 
 */
+
+int getServSockaddr(int port, struct sockaddr_in* serv);
+int getSocket(int port, struct sockaddr_in* serv);
+int ecoute(int fd);
+int envoie(int fd);
 
 int main(int argc, char* argv[]){
 
@@ -26,7 +34,45 @@ int main(int argc, char* argv[]){
 		}
 	}
 	
-	
-	
 	return EXIT_SUCCESS;
 }
+
+
+int getServSockaddr(int port, struct sockaddr_in* serv){
+	if(port<=1024)
+		return EXIT_FAILURE;
+		
+	serv->sin_family = AF_INET;
+	serv->sin_port = htons(port);
+	serv->sin_addr.s_addr = INADDR_ANY;
+	return EXIT_SUCCESS;
+}
+
+int envoie(int fd){
+	/* connect function ? */
+	return fd;
+}
+
+int ecoute(int fd){
+	if(listen(fd,2000) == EXIT_FAILURE){
+		perror("ecoute");
+		return EXIT_FAILURE;
+	}
+	
+	/* accept function ?*/
+	return fd;
+}
+
+int getSocket(int port, struct sockaddr_in* serv){
+	int fd = socket(AF_INET,SOCK_DGRAM,0);
+	
+	if(getServSockaddr(port,serv) == EXIT_FAILURE)
+		return EXIT_FAILURE;
+	
+	if(bind(fd,(struct sockaddr*)serv, sizeof(struct sockaddr_in)) == EXIT_FAILURE){
+		perror("getSocket");
+		return EXIT_FAILURE;
+	}
+	return fd;
+}
+
