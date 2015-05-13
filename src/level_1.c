50cea62622706060d04faa2a528774a3585b9753
damienChesneau <ches.damien@gmail.com>
2015-05-13 18:52:21 +0200

    thread in place !

--- src/level_1.c
+++ src/level_1.c
@@ -15,69 +15,70 @@ Level 1: Each repository has an execution of this program (multiple instances) a
  */
 int setAcceptSocket(struct sockaddr_in* serv, int socket_fd);
 int getSockAddr(int port, struct sockaddr_in* serv);
+const char* ip_addr = "127.0.0.1";
+int port_now = PORT;
 
-void *thread_1(void *arg)
-{
-    printf("We are in the thread.\n");
-
+void * switchman(void *arg) {
     (void) arg;
+    struct sockaddr_in addr;
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (getSockAddr(PORT , &addr) == EXIT_FAILURE) {
+        perror("GET SOCK ADDR");
+        return (void *) EXIT_FAILURE;
+    }
+    if (setAcceptSocket(&addr, socket_fd) == EXIT_FAILURE) {
+        perror("SET ACCEPT SOCK");
+        return (void *) EXIT_FAILURE;
+    }
+
+    while (1) {
+        printf("WAIT NEW CONNEXION...\n");
+        int newSock = accept(socket_fd, NULL, NULL);
+        pthread_t new_manager;
+
+        if (pthread_create(&new_manager, NULL, switchman, (void*)(intptr_t) newSock) == -1) {
+            perror("pthread_create");
+            return (void*) EXIT_FAILURE;
+        }
+    }
     pthread_exit(NULL);
 }
 
+void * connexion_manager(void *arg) {
+    int newSock = (intptr_t) arg;
+    if (newSock != EXIT_FAILURE) {
+        char buff[10];
+        read(newSock, buff, 10);
+        printf("%s\n", buff);
+    }
+    pthread_exit(NULL);
+}
 
 int main(int argc, char* argv[]) {
-    char* ip_addr = "127.0.0.1";
-    struct sockaddr_in addr;
-
     pthread_t thread1;
-
-    printf("brefore creating thread.\n");
-
-    if(pthread_create(&thread1, NULL, thread_1, NULL) == -1) {
-	perror("pthread_create");
-	return EXIT_FAILURE;
-    }
-    
-    printf("After created thread.\n");
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (getSockAddr(PORT + 1, &addr) == EXIT_FAILURE) {
-        perror(argv[0]);
+    if (pthread_create(&thread1, NULL, switchman, NULL) == -1) {
+        perror("pthread_create");
         return EXIT_FAILURE;
     }
-    if (argc < 2) {
-        if (setAcceptSocket(&addr, socket_fd) == EXIT_FAILURE) {
-            perror(argv[0]);
-            return EXIT_FAILURE;
-        }
-
-        int newSock = accept(socket_fd, NULL, NULL);
-        if (newSock != EXIT_FAILURE) {
-            char buff[10];
-            read(newSock, buff, 10);
-            printf("%s\n", buff);
-        }
-    } else {
-        if (inet_aton(ip_addr, &(addr.sin_addr.s_addr)) == -1) {
-            perror(argv[0]);
-            return EXIT_FAILURE;
-        }
-        if (connect(socket_fd, (struct sockaddr *) &addr, sizeof (struct sockaddr)) == EXIT_FAILURE) {
-            perror(argv[0]);
-            return EXIT_FAILURE;
-        }
-        write(socket_fd, ip_addr, 10);
+    while (1){
+        sleep(500000);
     }
-
-
+    /*        if (inet_aton(ip_addr, &(addr.sin_addr.s_addr)) == -1) {
+    //            perror(argv[0]);
+    //            return EXIT_FAILURE;
+    //        }
+    //        if (connect(socket_fd, (struct sockaddr *) &addr, sizeof (struct sockaddr)) == EXIT_FAILURE) {
+    //            perror(argv[0]);
+    //            return EXIT_FAILURE;
+    //        }
+    //        write(socket_fd, ip_addr, 10);*/
     return EXIT_SUCCESS;
 }
 
 int getSockAddr(int port, struct sockaddr_in* serv) {
-
     if (port <= 1024) {
         return EXIT_FAILURE;
     }
-
     serv->sin_family = AF_INET;
     serv->sin_port = htons(port);
     serv->sin_addr.s_addr = INADDR_ANY;
@@ -85,7 +86,6 @@ int getSockAddr(int port, struct sockaddr_in* serv) {
 }
 
 int setAcceptSocket(struct sockaddr_in* serv, int socket_fd) {
-
     if (bind(socket_fd, (struct sockaddr*) serv, sizeof (struct sockaddr_in)) == EXIT_FAILURE) {
         perror("getSockAddr");
         return EXIT_FAILURE;
